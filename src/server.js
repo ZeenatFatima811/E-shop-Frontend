2814e397a7634f23069ffa775fc417a8a887fe8b
Currency-Converter <z8784507@gmail.com>
2026-07-04 10:45:48 +0500

    Add backend deployed URL 1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 // export const server= "http://localhost:8080/api/v2";
 // export const backend_url="http://localhost:8080/uploads";
 
-const isProduction = import.meta.env.PROD;
+// const isProduction = import.meta.env.PROD;
 
-export const server = isProduction
-  ? "https://e-shop-backend-beta.vercel.app/api/v2"
-  : "http://localhost:8080/api/v2";
+// export const server = isProduction
+//   ? "https://e-shop-backend-beta.vercel.app/api/v2"
+//   : "http://localhost:8080/api/v2";
 
-export const backend_url = isProduction
-  ? "https://e-shop-backend-beta.vercel.app"
-  : "http://localhost:8080";
+// export const backend_url = isProduction
+//   ? "https://e-shop-backend-beta.vercel.app"
+//   : "http://localhost:8080";
+
+export const server = "https://e-shop-backend-beta.vercel.app/api/v2";
+export const backend_url = "https://e-shop-backend-beta.vercel.app";
